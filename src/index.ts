a730041306447da7c9bb9e2e551f24ea1833b3fe
senti <brianmv@uci.edu>
2023-06-28 14:17:37 -0700

    done, very epic terraform

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,11 @@ import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
 import { Image } from "@cdktf/provider-docker/lib/image";
 import { Container } from "@cdktf/provider-docker/lib/container";
 import { Manifest, type StackManifest, type TerraformStackMetadata } from "cdktf";
-import { Errors } from '@cdktf/commons'
+import { Errors, ensureAllSettledBeforeThrowing } from '@cdktf/commons'
 import {
   getMultipleStacks,
   getStackWithNoUnmetDependencies,
   checkIfAllDependenciesAreIncluded,
-  findAllNestedDependantStacks
 } from '@cdktf/cli-core/src/lib/helpers/stack-helpers'
 import { CdktfStack } from '@cdktf/cli-core';
 
@@ -67,6 +66,26 @@ function getDirectories(source: string) {
     : [];
 };
 
+export type MultipleStackOptions = {
+  stackNames?: string[];
+};
+
+export type AutoApproveOptions = {
+  autoApprove?: boolean;
+};
+
+export type MutationOptions = MultipleStackOptions &
+  AutoApproveOptions & {
+    refreshOnly?: boolean;
+    ignoreMissingStackDependencies?: boolean;
+    parallelism?: number;
+    terraformParallelism?: number;
+    vars?: string[];
+    varFiles?: string[];
+    noColor?: boolean;
+    migrateState?: boolean;
+  };
+
 async function start() {
   // const app = new App();
 
@@ -112,9 +131,7 @@ async function start() {
 
   const stackNames = stacks.map((s) => s.name);
 
-  const existingDirectories = getDirectories(
-    path.join(outdir, Manifest.stacksFolder)
-  );
+  const existingDirectories = getDirectories(path.join(outdir, Manifest.stacksFolder));
 
 
   const orphanedDirectories = existingDirectories.filter(
@@ -126,7 +143,10 @@ async function start() {
   }
 
   // deploy
-  let opts: any = {}
+  let opts = {
+    stackNames,
+    ignoreMissingStackDependencies: false,
+  }
 
   const stacksToRun = getMultipleStacks(stacks, opts.stackNames, "deploy");
 
@@ -134,8 +154,20 @@ async function start() {
     checkIfAllDependenciesAreIncluded(stacksToRun);
   }
 
+  const abortController = new AbortController();
+
   const thisstacksToRun = stacksToRun.map((stack) => {
-    return new CdktfStack({ ...opts, stack });
+    return new CdktfStack({
+      ...opts,
+      onUpdate(update) {
+        console.log({ update })
+        if (update.type === 'waiting for stack approval') {
+          update.approve()
+        }
+      },
+      abortSignal: abortController.signal,
+      stack,
+    });
   });
 
   // this.stopAllStacksThatCanNotRunWithout = (stackName: string) => {
@@ -145,6 +177,66 @@ async function start() {
   const next = opts.ignoreMissingStackDependencies
     ? () => Promise.resolve(thisstacksToRun.filter((stack) => stack.isPending)[0])
     : () => getStackWithNoUnmetDependencies(thisstacksToRun);
+
+  async function execute(
+    method: "deploy" | "destroy",
+    next: () => Promise<CdktfStack | undefined>,
+    opts: MutationOptions,
+  ) {
+    // We only support refresh only on deploy, a bit of a leaky abstraction here
+    if (opts.refreshOnly && method !== "deploy") {
+      throw Errors.Internal(`Refresh only is only supported on deploy`);
+    }
+
+    const maxParallelRuns = !opts.parallelism || opts.parallelism < 0 ? Infinity : opts.parallelism;
+
+    const allExecutions: Promise<unknown>[] = [];
+
+    // await this.initializeStacksToRunInSerial(opts.noColor);
+
+    // await Promise.all(
+    //   thisstacksToRun.map(stack => stack.initalizeTerraform(opts.noColor))
+    // )
+
+    while (thisstacksToRun.filter((stack) => stack.isPending).length > 0) {
+      const runningStacks = thisstacksToRun.filter((stack) => stack.isRunning);
+
+      if (runningStacks.length >= maxParallelRuns) {
+        await Promise.race(runningStacks.map((s) => s.currentWorkPromise));
+        continue;
+      }
+      try {
+        const nextRunningExecutor = await next();
+        if (!nextRunningExecutor) {
+          // In this case we have no pending stacks, but we also can not find a new executor
+          break;
+        }
+        const promise =
+          method === "deploy"
+            ? nextRunningExecutor.deploy(opts)
+            : nextRunningExecutor.destroy(opts);
+
+        allExecutions.push(promise);
+      } catch (e) {
+        // await next() threw an error because a stack failed to apply/destroy
+        // wait for all other currently running stacks to complete before propagating that error
+        console.debug("Encountered an error while awaiting stack to finish", e);
+        const openStacks = thisstacksToRun.filter((ex) => ex.currentWorkPromise);
+        console.debug("Waiting for still running stacks to finish:", openStacks);
+        await Promise.allSettled(openStacks.map((ex) => ex.currentWorkPromise));
+        console.debug("Done waiting for still running stacks. All pending work finished");
+        throw e;
+      }
+    }
+
+    // We end the loop when all stacks are started, now we need to wait for them to be done
+    // We wait for all work to finish even if one of the promises threw an error.
+    await ensureAllSettledBeforeThrowing(
+      Promise.all(allExecutions),
+      allExecutions
+    );
+  }
+
 
   await execute("deploy", next, opts);
 
