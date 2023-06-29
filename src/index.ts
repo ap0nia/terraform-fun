82bb723509d3b0d364ddf0804366cd40123e7bdd
senti <brianmv@uci.edu>
2023-06-28 21:25:12 -0700

    removed extraneous docker

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,8 @@
+import { aws_s3 } from 'aws-cdk-lib';
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
-import { aws_s3 } from 'aws-cdk-lib';
 import { AwsTerraformAdapter, provider } from "@cdktf/aws-cdk";
-
-import { DockerProvider } from "@cdktf/provider-docker/lib/provider";
-import { Image } from "@cdktf/provider-docker/lib/image";
-import { Container } from "@cdktf/provider-docker/lib/container";
 import { CdktfProject } from './cdktf/project.js'
-
-export class MyStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
-
-    new DockerProvider(this, "docker", {});
-
-    const dockerImage = new Image(this, "nginxImage", {
-      name: "nginx:latest",
-      keepLocally: false,
-    });
-
-    new Container(this, "nginxContainer", {
-      name: "tutorial",
-      image: dockerImage.name,
-      ports: [
-        {
-          internal: 80,
-          external: 8000,
-        }
-      ],
-    });
-  }
-}
 
 export class HelloCdkStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -44,11 +16,9 @@ export class HelloCdkStack extends TerraformStack {
   }
 }
 
-
 async function synthFn() {
   const app = new App();
 
-  // new MyStack(app, "learn-cdktf-docker");
   new HelloCdkStack(app, "learn-cdktf-aws");
 
   app.synth();
@@ -58,6 +28,7 @@ async function start() {
   const project = new CdktfProject({ synthFn });
 
   // await project.deploy()
+
   await project.destroy()
 }
 
