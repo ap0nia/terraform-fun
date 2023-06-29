362a4800278d2be287602057f2e8faf85614d689
senti <brianmv@uci.edu>
2023-06-29 13:54:52 -0700

    just work

--- src/index.ts
+++ src/index.ts
@@ -30,53 +30,9 @@ async function synthFn() {
 async function start() {
   const project = new CdktfProject({ synthFn });
 
-  // await project.get()
+  await project.deploy()
 
-  // await project.deploy()
-
-  await project.destroy()
+  // await project.destroy()
 }
 
 start()
-
-// import { Construct } from "constructs";
-// import { App, TerraformStack } from "cdktf";
-// import { aws_s3 } from 'aws-cdk-lib';
-// import { AwsTerraformAdapter, provider } from "@cdktf/aws-cdk";
-// import { CdktfProject } from './cdktf/project.js'
-// 
-// export class HelloCdkStack extends TerraformStack {
-//   constructor(scope: Construct, name: string) {
-//     super(scope, name);
-// 
-//     new provider.AwsProvider(this, 'aws', { region: 'us-east-1' })
-// 
-//     const awsAdapter = new AwsTerraformAdapter(this, "adapter");
-// 
-//     new aws_s3.Bucket(awsAdapter, 'MyFirstBucket', { 
-//       bucketName: 'my-first-bucket-in-terraform-is-so-epic',
-//       versioned: true,
-//     });
-//   }
-// }
-// 
-// 
-// async function synthFn() {
-//   const app = new App();
-// 
-//   // new MyStack(app, "learn-cdktf-docker");
-//   new HelloCdkStack(app, "learn-cdktf-aws");
-// 
-//   app.synth();
-// }
-// 
-// synthFn()
-
-// async function start() {
-//   const project = new CdktfProject({ synthFn });
-// 
-//   await project.deploy()
-//   await project.destroy()
-// }
-// 
-// start()
