4d57569ea78f077600b21c8b6859f27c2a4a4396
senti <brianmv@uci.edu>
2023-06-28 22:32:03 -0700

    automatic deploy container thingy

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import { aws_s3 } from 'aws-cdk-lib';
 import { Construct } from "constructs";
+import aws_s3 from 'aws-cdk-lib/aws-s3';
+import aws_ecr from 'aws-cdk-lib/aws-ecr';
 import { App, TerraformStack } from "cdktf";
 import { AwsTerraformAdapter, provider } from "@cdktf/aws-cdk";
 import { CdktfProject } from './cdktf/project.js'
+import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import ecrdeploy from 'cdk-ecr-deployment';
 
 export class HelloCdkStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -11,6 +14,18 @@ export class HelloCdkStack extends TerraformStack {
     new provider.AwsProvider(this, 'aws', { region: 'us-east-1' })
 
     const awsAdapter = new AwsTerraformAdapter(this, "adapter");
+
+    const repository = new aws_ecr.Repository(awsAdapter, 'MyFirstRepository', {
+      imageScanOnPush: true,
+      imageTagMutability: aws_ecr.TagMutability.MUTABLE
+    })
+
+    const image = new DockerImageAsset(awsAdapter, 'MyFirstImage', { directory: '.' });
+
+    new ecrdeploy.ECRDeployment(awsAdapter, 'MyFirstImageDeployment', {
+      src: new ecrdeploy.DockerImageName(image.imageUri),
+      dest: new ecrdeploy.DockerImageName(`${repository.repositoryUri}:nginx`),
+    });
 
     new aws_s3.Bucket(awsAdapter, 'MyFirstBucket', { versioned: true });
   }
@@ -27,9 +42,9 @@ async function synthFn() {
 async function start() {
   const project = new CdktfProject({ synthFn });
 
-  // await project.deploy()
+  await project.deploy()
 
-  await project.destroy()
+  // await project.destroy()
 }
 
 start()
