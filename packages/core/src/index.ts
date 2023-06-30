4f8ce77610e2448a530a38b59b9590ed2f98eaa7
senti <brianmv@uci.edu>
2023-06-30 10:58:43 -0700

    why not work

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,10 +1,34 @@
 import { Construct } from "constructs";
-import aws_s3 from 'aws-cdk-lib/aws-s3';
-import { App, TerraformStack, S3Backend } from "cdktf";
-import { AwsTerraformAdapter, provider } from "@cdktf/aws-cdk";
+import cdk from 'aws-cdk-lib'
+import { App, TerraformStack, S3Backend, TerraformAsset, AssetType } from "cdktf";
+import * as aws from "@cdktf/provider-aws";
+import { provider } from "@cdktf/aws-cdk";
 import { CdktfProject } from './cdktf/project.js'
+import { searchForWorkspaceRoot } from './utils/directories.js'
 
 const bucketName = 'cdktf-state'
+
+const functionName = 'hello-cdk'
+
+const workspaceRoot = searchForWorkspaceRoot(process.cwd())
+
+const directory = `${workspaceRoot}/packages/server`
+
+// const restApiName = `${bucketName}-api`
+
+const lambdaRolePolicy = {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+};
 
 /**
  * Needs to exist first.
@@ -13,11 +37,11 @@ export class CdktfStateStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    new provider.AwsProvider(this, 'aws', { region: 'us-east-1' })
+    // new provider.AwsProvider(this, 'aws', { region: 'us-east-1' })
 
-    const awsAdapter = new AwsTerraformAdapter(this, "adapter");
+    // const awsAdapter = new AwsTerraformAdapter(this, "adapter");
 
-    new aws_s3.Bucket(awsAdapter, 'MyFirstBucket', { bucketName, versioned: true });
+    // new cdk.aws_s3.Bucket(awsAdapter, 'MyFirstBucket', { bucketName, versioned: true });
   }
 }
 
@@ -25,22 +49,89 @@ export class HelloCdkStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    new provider.AwsProvider(this, 'aws', { 
+    new provider.AwsProvider(this, 'aws', {
       region: process.env.AWS_REGION ?? 'us-east-1'
     })
 
-    new S3Backend(this, { 
+    // const awsAdapter = new AwsTerraformAdapter(this, "adapter");
+
+    new S3Backend(this, {
       bucket: bucketName,
       key: 'terraform.tfstate',
       region: process.env.AWS_REGION ?? 'us-east-1'
     })
 
-    const awsAdapter = new AwsTerraformAdapter(this, "adapter");
-
-    new aws_s3.Bucket(awsAdapter, 'MyFirstBucket', {
-      bucketName: `${name}-bucket`,
-      versioned: true
+    // Create Lambda executable
+    const asset = new TerraformAsset(this, "lambda-asset", { 
+      path: directory,
+      type: AssetType.ARCHIVE,
     });
+
+    // Create unique S3 bucket that hosts Lambda executable
+    const bucket = new aws.s3Bucket.S3Bucket(this, "bucket", {
+      bucketPrefix: `learn-cdktf-${name}`,
+    });
+
+    // Upload Lambda zip file to newly created S3 bucket
+    const lambdaArchive = new aws.s3Object.S3Object(this, "lambda-archive", {
+      bucket: bucket.bucket,
+      key: `${functionName}.zip`,
+      source: asset.path, // returns a posix path
+    });
+
+    // Create Lambda role
+    const role = new aws.iamRole.IamRole(this, "lambda-exec", {
+      name: `${functionName}-exec`,
+      assumeRolePolicy: JSON.stringify(lambdaRolePolicy)
+    });
+
+    // Add execution role for lambda to write to CloudWatch logs
+    new aws.iamRolePolicyAttachment.IamRolePolicyAttachment(this, "lambda-managed-policy", {
+      policyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole',
+      role: role.name
+    });
+
+    // const resource = new cdk.aws_apigateway.RestApi(awsAdapter, restApiName, {
+    //   defaultCorsPreflightOptions: {
+    //     allowOrigins: ["*"],
+    //     allowHeaders: ["Apollo-Require-Preflight", "Content-Type"],
+    //     allowMethods: ["GET", "HEAD", "POST"],
+    //   },
+    //   endpointTypes: [cdk.aws_apigateway.EndpointType.EDGE],
+    //   minCompressionSize: cdk.Size.bytes(128 * 1024), // 128 KiB
+    //   restApiName,
+    // })
+
+    const lambdaFunc = new aws.lambdaFunction.LambdaFunction(this, `${functionName}-handler`, {
+      functionName,
+      s3Bucket: bucket.bucket,
+      s3Key: lambdaArchive.key,
+      runtime: cdk.aws_lambda.Runtime.NODEJS_18_X.toString(),
+      handler: 'dist/index.handler',
+      architectures: [cdk.aws_lambda.Architecture.ARM_64.name],
+      timeout: cdk.Duration.seconds(15).toSeconds(),
+      memorySize: 512,
+      role: role.arn,
+      // code: cdk.aws_lambda.Code.fromAsset(directory, { exclude: ['node_modules'] }),
+      // architecture: cdk.aws_lambda.Architecture.ARM_64.name,
+    })
+
+    // resource.root.addMethod('GET', new cdk.aws_apigateway.LambdaIntegration(handler))
+
+    // Create and configure API gateway
+    const api = new aws.apigatewayv2Api.Apigatewayv2Api(this, "api-gw", {
+      name: name,
+      protocolType: "HTTP",
+      target: lambdaFunc.arn
+    });
+
+    new aws.lambdaPermission.LambdaPermission(this, "apigw-lambda", {
+      functionName: lambdaFunc.functionName,
+      action: "lambda:InvokeFunction",
+      principal: "apigateway.amazonaws.com",
+      sourceArn: `${api.executionArn}/*/*`,
+    });
+
   }
 }
 
@@ -53,11 +144,11 @@ async function synthFn() {
 }
 
 async function start() {
-  const project = new CdktfProject({ synthFn });
+  // const project = new CdktfProject({ synthFn });
 
   // await project.deploy()
 
-  await project.destroy()
+  // await project.destroy()
 }
 
 start()
--- packages/core/src/utils/directories.ts
+++ packages/core/src/utils/directories.ts
@@ -10,3 +10,91 @@ export function getDirectories(source: string) {
     : [];
 };
 
+
+export function isFileReadable(filename: string): boolean {
+  try {
+    // The "throwIfNoEntry" is a performance optimization for cases where the file does not exist
+    if (!fs.statSync(filename, { throwIfNoEntry: false })) {
+      return false;
+    }
+
+    // Check if current process has read permission to the file
+    fs.accessSync(filename, fs.constants.R_OK);
+
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+// https://github.com/vitejs/vite/issues/2820#issuecomment-812495079
+const ROOT_FILES = [
+  // '.git',
+
+  // https://pnpm.io/workspaces/
+  "pnpm-workspace.yaml",
+
+  // https://rushjs.io/pages/advanced/config_files/
+  // 'rush.json',
+
+  // https://nx.dev/latest/react/getting-started/nx-setup
+  // 'workspace.json',
+  // 'nx.json',
+
+  // https://github.com/lerna/lerna#lernajson
+  "lerna.json",
+];
+
+// npm: https://docs.npmjs.com/cli/v7/using-npm/workspaces#installing-workspaces
+// yarn: https://classic.yarnpkg.com/en/docs/workspaces/#toc-how-to-use-it
+export function hasWorkspacePackageJSON(root: string): boolean {
+  const currentDirectoryPackageJson = path.join(root, "package.json");
+
+  if (!isFileReadable(currentDirectoryPackageJson)) {
+    return false;
+  }
+
+  const content = JSON.parse(fs.readFileSync(currentDirectoryPackageJson, "utf-8")) || {};
+  return !!content.workspaces;
+}
+
+export function hasRootFile(root: string): boolean {
+  return ROOT_FILES.some((file) => fs.existsSync(path.join(root, file)));
+}
+
+export function hasPackageJSON(root: string) {
+  const currentDirectoryPackageJson = path.join(root, "package.json");
+  return fs.existsSync(currentDirectoryPackageJson);
+}
+
+/**
+ * Search up for the nearest `package.json`, i.e. the current project root.
+ */
+export function searchForProjectDirectory(current: string, root = current): string {
+  if (hasPackageJSON(current)) return current;
+
+  const currentDirectory = path.dirname(current);
+
+  // reach the fs root
+  if (!currentDirectory || currentDirectory === current) return root;
+
+  return searchForProjectDirectory(currentDirectory, root);
+}
+
+/**
+ * Search up for the nearest workspace root.
+ */
+export function searchForWorkspaceRoot(
+  current: string,
+  root = searchForProjectDirectory(current)
+): string {
+  if (hasRootFile(current)) return current;
+  if (hasWorkspacePackageJSON(current)) return current;
+
+  const currentDirectory = path.dirname(current);
+
+  // reach the fs root
+  if (!currentDirectory || currentDirectory === current) return root;
+
+  return searchForWorkspaceRoot(currentDirectory, root);
+}
