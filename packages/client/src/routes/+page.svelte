3fcd1810db80a46848a2e96284bd9f3531d67ca7
senti <brianmv@uci.edu>
2023-06-30 21:31:27 -0700

    it works!

---
+++ packages/client/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let data: any
+
+  async function handleClick() {
+    const response = await fetch('https://3z5i1laxlc.execute-api.us-east-1.amazonaws.com/')
+    data = await response.json()
+  }
+</script>
+
+<button type="button" on:click={handleClick}>Trigger a workflow!</button>
+
+<h1>Response Data...</h1>
+
+<pre>{JSON.stringify(data, null, 2)}</pre>
