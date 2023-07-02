f42ccddc1b8fb0cf267e1f794c12959e290001b4
senti <brianmv@uci.edu>
2023-07-02 06:10:49 -0700

    feat: created pubsubhubub class

--- packages/core/src/lambda.ts
+++ packages/core/src/lambda.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import type { ProxyHandler } from 'aws-lambda'
 
 /**
- * esbuild will pick up on this and copy the env file to the output folder.
+ * ESBuild will pick up on this and copy the env file to the output folder.
  */
 try {
   require("../.env");
@@ -11,23 +11,67 @@ try {
   /* noop */
 }
 
+interface SubscriptionData {
+  topic: string
+  callback: string
+  token: string
+}
+
+class PubSubHubbub {
+  constructor(private readonly hub: string) { }
+
+  async subscribe(data: SubscriptionData) {
+    const body = new FormData()
+
+    body.append('hub.mode', 'subscribe')
+    body.append('hub.topic', data.topic)
+    body.append('hub.callback', data.callback)
+
+    const response = await fetch(this.hub, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${data.token}`,
+      },
+      body
+    })
+
+    return response
+  }
+
+  async unsubscribe(data: SubscriptionData) {
+    const body = new FormData()
+
+    body.append('hub.mode', 'unsubscribe')
+    body.append('hub.topic', data.topic)
+    body.append('hub.callback', data.callback)
+
+    const response = await fetch(this.hub, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${data.token}`,
+      },
+      body
+    })
+
+    return response
+  }
+}
+
+
 export const handler: ProxyHandler = async () => {
   const owner = 'ap0nia'
   const repo = 'terraform-fun'
   const authorization = ''
 
-  const body = new FormData()
-  body.append('hub.mode', 'subscribe')
-  body.append('hub.topic', `https://github.com/${owner}/${repo}/events/issues.json`)
-  body.append('hub.callback', 'https://9be6-104-7-144-91.ngrok.io')
+  const pubsub = new PubSubHubbub('https://api.github.com/hub')
 
-  const response = await fetch('https://api.github.com/hub', {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${authorization}`,
-    },
-    body
-  })
+  const data: SubscriptionData = {
+    topic: `https://github.com/${owner}/${repo}/events/issues.json`,
+    callback: 'https://9be6-104-7-144-91.ngrok.io',
+    token: authorization
+  }
+
+  const response = await pubsub.subscribe(data)
 
   return {
     statusCode: 200,
